c8ab761417a48f3f904c3e4d1a49af0fa83be01c
Andrew Lamb <andrew@nerdnetworks.org>
2021-11-02 10:34:01 -0400

    initial

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # oom_canary
 Test what happens when cargo test blows up with OOM on github actions
+
+
+## Question:
+What is the symptom when `cargo test` exceeds its memory limits on github runner?
+
+
+TODO
+```
+cargo test
+```
---
+++ oom/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+
+    const MEMORY_LIMIT: usize = 8_000_000_000;
+    //const MEMORY_LIMIT: usize = 32_000_000;
+
+    #[test]
+    fn oom() {
+        // allocate 32GB of memory and hope for the best
+        println!("Starting to allocate {}", MEMORY_LIMIT);
+        let mut t : Vec<usize> = vec![];
+
+        let a = vec![0; 100000];
+
+        println!("Allocated; Starting to write {}", MEMORY_LIMIT);
+        for i in 0..MEMORY_LIMIT {
+            t.extend(a.iter());
+        }
+        println!("Done");
+    }
+
+}
