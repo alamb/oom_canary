8c54f3f74bb1f717c6cecbffa466ceb82317b255
Andrew Lamb <andrew@nerdnetworks.org>
2021-11-02 11:25:03 -0400

    twaks

--- oom/src/main.rs
+++ oom/src/main.rs
@@ -8,19 +8,22 @@ fn main() {
 #[cfg(test)]
 mod tests {
 
-    const MEMORY_LIMIT: usize = 8_000_000_000;
-    //const MEMORY_LIMIT: usize = 32_000_000;
+    //const MEMORY_LIMIT: usize = 8_000_000_000;
+    const MEMORY_LIMIT: usize = 32_000_000_000;
 
     #[test]
     fn oom() {
         // allocate 32GB of memory and hope for the best
         println!("Starting to allocate {}", MEMORY_LIMIT);
-        let mut t : Vec<usize> = vec![];
-        let a = vec![0; 100000];
+        let mut t : Vec<_> = vec![];
+        let a: Vec<usize> = vec![0; 100000];
 
         println!("Allocated; Starting to write {}", MEMORY_LIMIT);
-        for i in 0..MEMORY_LIMIT {
-            t.extend(a.iter());
+        let mut total = 0;
+        while total < MEMORY_LIMIT {
+            let a = a.clone();
+            total += std::mem::size_of::<Vec<usize>>() + (a.capacity() * std::mem::size_of::<usize>());
+            t.push(a);
         }
         println!("Done");
     }
